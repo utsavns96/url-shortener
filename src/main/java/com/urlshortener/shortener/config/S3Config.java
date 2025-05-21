59e712a2e8b0b4b3477200f5e5a4b613384290d4
Utsav <utsavns96@gmail.com>
2025-05-21 16:35:20 -0500

    Fixing region in S3Config.java

--- src/main/java/com/urlshortener/shortener/config/S3Config.java
+++ src/main/java/com/urlshortener/shortener/config/S3Config.java
@@ -2,14 +2,16 @@ package com.urlshortener.shortener.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 
 @Configuration
 public class S3Config {
     @Bean
-    public S3Client s3Client()
-    {
-        return S3Client.create();
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .region(Region.US_EAST_1) // Replace with your region
+                .build();
     }
 }
