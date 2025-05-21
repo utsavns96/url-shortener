5bfb474d95ad78ace85a462c1363190e6f886360
Utsav <utsavns96@gmail.com>
2025-05-21 16:19:30 -0500

    Fixing project structure

--- shortener/src/main/java/com/urlshortener/shortener/service/UrlShortenerService.java
+++
@@ -1,8 +0,0 @@
-package com.urlshortener.shortener.service;
-
-import java.util.Optional;
-
-public interface UrlShortenerService {
-    String createShortUrl(String originalURL);
-    Optional<String> getOriginalUrl(String shortURL);
-}
